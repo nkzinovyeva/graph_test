998a4509ddd234999943c6124a01ed7e37ac2ad4
Nataliya Zinovyeva <52114410+nkzinovyeva@users.noreply.github.com>
2020-11-13 20:04:29 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,200 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+//import ApolloClient from 'apollo-boost';
+//import { ApolloProvider} from '@apollo/react-hooks';
+import { gql } from "apollo-boost";
+import { useQuery } from '@apollo/react-hooks';
 
 function App() {
-  return (
+  
+  const countriesquery = gql`
+  {
+    countries {
+      code,
+      name,
+      continent {name}
+    }
+  }`;
+
+  const { loading, error, data } = useQuery(countriesquery);
+
+  if (loading) // If still loading
+    return "Loading...";
+  if (error) // It there came an error
+    return `Error ${error.message}`;
+
+  return ( // When there is data
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table>
+        <tr>
+          <th>Code</th>
+          <th>Country</th>
+          <th>Continent</th>
+        </tr>
+        <tbody>
+          {
+            data.countries.map((country, index) => 
+              
+              <tr key={index}>
+                <td>{country.code}</td>
+                <td>{country.name}</td>
+                <td>{country.continent.name}</td>
+              </tr>
+            )
+          }
+        </tbody>
+      </table>
     </div>
   );
 }
+/*  
+
+===== Companies query =======
+
+
+const companiesquery = gql`
+  {
+    companies {
+      name,
+      websiteUrl
+    }
+  }`;
+
+  const { loading, error, data } = useQuery(companiesquery);
+
+  if (loading) // If still loading
+    return "Loading...";
+  if (error) // It there came an error
+    return `Error ${error.message}`;
+
+  return ( // When there is data
+    <div className="App">
+      <table>
+        <tbody>
+          {
+            data.companies.map((company, index) => 
+              <tr key={index}>
+                <td>{company.name}</td>
+                <td><a href={company.websiteUrl}>{company.websiteUrl}</a></td>
+              </tr>
+            )
+          }
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+===== Cities query =======
+
+const cityquery = gql`
+  {
+    cities {
+      name,
+      country {name},
+    }
+  }`;
+
+  const { loading, error, data } = useQuery(cityquery);
+
+  if (loading) // If still loading
+    return "Loading...";
+  if (error) // It there came an error
+    return `Error ${error.message}`;
+
+  return ( // When there is data
+    <div className="App">
+      <table>
+        <tbody>
+          {
+            data.cities.map((city, index) => 
+              <tr key={index}>
+                <td>{city.name}</td>
+                <td>{city.country.name}</td>
+              </tr>
+            )
+          }
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+===== Jobs query =======
+
+const jobquery = gql`
+  {
+    jobs { 
+      title, 
+      company{name}, 
+      applyUrl, 
+    }
+  }`;
+
+  const { loading, error, data } = useQuery(jobquery);
+
+  if (loading) // If still loading
+    return "Loading...";
+  if (error) // It there came an error
+    return `Error ${error.message}`;
+
+  return ( // When there is data
+    <div className="App">
+      <table>
+        <tbody>
+          {
+            data.jobs.map((job, index) => 
+              <tr key={index}>
+                <td>{job.title}</td>
+                <td>{job.company.name}</td>
+                <td><a href={job.applyUrl}>{job.applyUrl}</a></td>
+              </tr>
+            )
+          }
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+===== First query =======
+
+  const client = new ApolloClient({
+    uri: 'https://api.graphql.jobs',
+  });
+
+  const[jobs, setJobs] = useState([]);
+
+  client
+  .query({
+    query: gql`
+    {
+      jobs {
+        title,
+        company{name},
+        applyUrl,
+      }
+    }
+    `
+  })
+  .then(result=> setJobs(result.data   .jobs));
+
+  return (
+    <div className="App">
+      <table>
+        <tbody>{
+          jobs.map((job, index) =>
+            <tr key = {index}>
+              <td>{job.title}</td>
+              <td>{job.company.name}</td>
+              <td><a href={job.applyUrl}>{job.applyUrl}</a></td>
+            </tr >
+          )
+        }</ tbody>
+        </table>
+    </div>
+  );
+}
+*/
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,16 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+//import * as serviceWorker from './serviceWorker';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const client = new ApolloClient({
+  uri: 'https://countries.trevorblades.com/',
+});
+
+ReactDOM.render(<ApolloProvider client={client}><App /></ApolloProvider>, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+//serviceWorker.unregister();
+
+/* https://api.graphql.job */
